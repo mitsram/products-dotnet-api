62bafe25ac54491adb5c946ca712bf4a8b6bdcc7
Mitchell Ramos <mitchell.ramos@aurizon.com.au>
2023-08-16 10:46:51 +1000

    Added openapi swagger

--- src/Products.Api/Controllers/ErrorsController.cs
+++ src/Products.Api/Controllers/ErrorsController.cs
@@ -5,6 +5,7 @@ namespace Products.Api.Controllers;
 public class ErrorsController : ControllerBase
 {
     [Route("/error")]
+    [ApiExplorerSettings(IgnoreApi = true)]
     public IActionResult Error()
     {
         return Problem();
--- src/Products.Api/Program.cs
+++ src/Products.Api/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.Extensions.Options;
+using Microsoft.OpenApi.Models;
+using Microsoft.VisualBasic;
 using Products.Application;
 using Products.Infrastructure;
 
@@ -8,12 +11,23 @@ var builder = WebApplication.CreateBuilder(args);
         .AddApplication()
         .AddInfrastructure();
     builder.Services.AddControllers();
+    builder.Services.AddSwaggerGen(options => {
+        var info = new OpenApiInfo{ Title = "My Api", Version = "v1" };
+        options.SwaggerDoc(name: "v1", info: info);
+    }
+    );
 }
 
 var app = builder.Build();
 {
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    }
     app.UseExceptionHandler("/error");
     app.UseHttpsRedirection();
     app.MapControllers();
+    
     app.Run();
 }
